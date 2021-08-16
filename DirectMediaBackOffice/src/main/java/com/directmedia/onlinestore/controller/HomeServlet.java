f4ff6bca54932ea5703c678752e9ea12d7b16c9a
Miklange <michael.choquet@gmail.com>
2021-08-16 17:54:46 +0200

    Commit du projet Back Office

---
+++ DirectMediaBackOffice/src/main/java/com/directmedia/onlinestore/controller/HomeServlet.java
@@ -0,0 +1,27 @@
+package com.directmedia.onlinestore.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HomeServlet
+ */
+@WebServlet( name = "HomeServlet", urlPatterns = { "/home" } )
+public class HomeServlet extends HttpServlet
+{
+
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doGet( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException
+    {
+        PrintWriter out = response.getWriter();
+        out.print( "<HTML><BODY> Bonjour de la Servlet Back</BODY></HTML>" );
+    }
+}
